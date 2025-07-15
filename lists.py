3efd23e44d3a95d6d271fec45ba0459ccae55928
Ainash <aakmatova179@gmail.com>
2025-07-15 08:01:27 +0600

    second commit

---
+++ lists.py
@@ -0,0 +1,8 @@
+names=["Harry","Ron","Hermione","Ginny"]
+print(names[0])
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
---
+++ sequences.py
@@ -0,0 +1,6 @@
+# name="Harry"
+# print(name[1])
+
+# names=["Arsen","Abbas","Abdulla"]
+# print(names[1])
+
